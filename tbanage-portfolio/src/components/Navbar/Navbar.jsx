a11a4fe6879e72fc34796d714a219a389f34665b
Tanmay Narayan Banage <tbanage@j5fkwj5wqm.agoda.local>
2023-12-09 15:53:25 +0530

    updating navbar scrolls

--- tbanage-portfolio/src/components/Navbar/Navbar.jsx
+++ tbanage-portfolio/src/components/Navbar/Navbar.jsx
@@ -12,6 +12,13 @@ const Navbar = () => {
     const fontColor = useColorModeValue('#58585e', '#f0e7db');
     const linkHoverColor = useColorModeValue("#ff63c3", "#88ccca");
 
+    const ScrollOffsets = [
+        { id: 'home', offset: -70 },
+        { id: 'experience', offset: 0 },
+        { id: 'projects', offset: -350 },
+        { id: 'contact', offset: -360 },
+    ]
+
     return (
         <Flex
             as="nav"
@@ -32,26 +39,30 @@ const Navbar = () => {
                 <Box as="li" color={fontColor}>
                     <span>Tanmay Banage</span>
                 </Box>
-                {['home', 'experience', 'projects', 'contact'].map((item, index) => (
-                    <Box as="li" key={index} mx="10px" color={fontColor}>
-                        <ScrollLink
-                            activeClass="active"
-                            to={item}
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                        >
-                            <Link
-                                color={fontColor}
-                                _hover={{ color: linkHoverColor }}
-                                _activeLink={{ color: linkHoverColor }}
+                {ScrollOffsets.map((item, index) => {
+                    const {id, offset} = item;
+                    return(
+                        <Box as="li" key={index} mx="10px" color={fontColor}>
+                            <ScrollLink
+                                activeClass="active"
+                                to={id}
+                                spy={true}
+                                smooth={true}
+                                offset={offset}
+                                duration={500}
                             >
-                                {item.charAt(0).toUpperCase() + item.slice(1)}
-                            </Link>
-                        </ScrollLink>
-                    </Box>
-                ))}
+                                <Link
+                                    color={fontColor}
+                                    _hover={{ color: linkHoverColor }}
+                                    _activeLink={{ color: linkHoverColor }}
+                                >
+                                    {id.charAt(0).toUpperCase() + id.slice(1)}
+                                </Link>
+                            </ScrollLink>
+                        </Box>
+                    )
+                    // console.log(item.id, item.value)
+                })}
                 <Box  as="li">
                     <Link color={fontColor} display="inline-flex" alignItems="center" href="https://github.com/xnsei/portfolio/tree/main/tbanage-portfolio" isExternal>
                         <IoLogoGithub />
